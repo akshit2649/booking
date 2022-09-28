061e88476a12fefbf87120c943a54e905338543f
Akshit Thakur <akshit2649@gmail.com>
2022-09-28 11:33:14 +0530

    maillist

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Featured from '../../components/featured/Featured';
 import FeaturedProperties from '../../components/featuredproperties/FeaturedProperties';
 import Header from '../../components/header/Header';
+import MailList from '../../components/maillist/MailList';
 import Navbar from '../../components/navbar/Navbar';
 import PropertyList from '../../components/propertylist/PropertyList';
 import './Home.css';
@@ -16,6 +17,7 @@ const Home = () => {
         <PropertyList />
         <h1 className="homeTitle">Home guests love</h1>
         <FeaturedProperties />
+        <MailList />
       </div>
     </div>
   );
