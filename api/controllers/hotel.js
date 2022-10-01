3a927982a95985af4a17703c8b2ca93dd339fa28
Akshit Thakur <akshit2649@gmail.com>
2022-10-01 22:18:08 +0530

    hotel controller

---
+++ api/controllers/hotel.js
@@ -0,0 +1,54 @@
+import { createError } from '../utils/error.js';
+import Hotel from '../models/Hotel.js ';
+
+export const createHotel = async (req, res, next) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    next(createError);
+  }
+};
+
+export const updateHotel = async (req, res, next) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    next(createError);
+  }
+};
+
+export const deleteHotel = async (req, res, next) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json('Hotel has been deleted!');
+  } catch (err) {
+    next(createError);
+  }
+};
+
+export const getHotel = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    next(createError);
+  }
+};
+
+export const getHotels = async (req, res, next) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (err) {
+    nexr(createError);
+  }
+};
