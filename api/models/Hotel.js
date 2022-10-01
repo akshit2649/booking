7e0fd78891dc03b3125bc57a91b8150387fa86e6
Akshit Thakur <akshit2649@gmail.com>
2022-10-01 19:18:43 +0530

    pushed first collection

--- api/models/Hotel.js
+++ api/models/Hotel.js
@@ -25,6 +25,10 @@ const HotelSchema = new Schema({
   photos: {
     type: [String],
   },
+  title: {
+    type: String,
+    required: true,
+  },
   desc: {
     type: String,
     required: true,
