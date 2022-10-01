2ed7c2d47a6008bb4f60dc55c415e1608ee3540d
Akshit Thakur <akshit2649@gmail.com>
2022-10-01 18:24:04 +0530

    removed env

--- api/index.js
+++ api/index.js
@@ -1,8 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('connected to mongoDB');
+  } catch (error) {
+    throw error;
+  }
+};
+
+//emitters
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
+});
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected');
+});
+
 app.listen(8800, () => {
+  connect();
   console.log('Connected to backend');
 });
