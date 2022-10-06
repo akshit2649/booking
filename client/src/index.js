d077b0b9590af9d349c9e557dd1d27f79caf346b
Akshit Thakur <akshit2649@gmail.com>
2022-10-07 03:19:26 +0530

    login

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 // import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
 import { SearchContextProvider } from './context/SearchContext';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SearchContextProvider>
-      <App />
-    </SearchContextProvider>
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
