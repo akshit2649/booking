64f4bc18d787c7d81c1cf3412c4cf822baa886bf
Akshit Thakur <akshit2649@gmail.com>
2022-10-10 00:32:03 +0530

    dynamic table generation

--- admin/src/App.js
+++ admin/src/App.js
@@ -9,6 +9,7 @@ import './style/dark.scss';
 import { useContext } from 'react';
 import { DarkModeContext } from './context/darkModeContext';
 import { AuthContext } from './context/AuthContext';
+import { hotelColumns, userColumns } from './datatablesource';
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
@@ -42,7 +43,7 @@ function App() {
                 index
                 element={
                   <ProtectedRoute>
-                    <List />
+                    <List columns={userColumns} />
                   </ProtectedRoute>
                 }
               />
@@ -56,12 +57,12 @@ function App() {
                 }
               />
             </Route>
-            <Route path="products">
+            <Route path="hotels">
               <Route
                 index
                 element={
                   <ProtectedRoute>
-                    <List />
+                    <List columns={hotelColumns} />
                   </ProtectedRoute>
                 }
               />
